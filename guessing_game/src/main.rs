e2af14c34f2db180affc0f436f5f9674d9717212
Jeremiah <j.munson225@gmail.com>
2020-01-07 11:56:17 -0700

    Ask for user guess, read line from terminal, and print guess.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+// Standard library input output
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // let is how variables are initialized
+    // mut means the variable is mutable (can be changed)
+    let mut guess = String::new();
+
+    // Reading in a line from the terminal
+    // the .expect() is for handling potential failure
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    // Printing a string and using a placeholder
+    println!("You guessed: {}", guess);
 }
