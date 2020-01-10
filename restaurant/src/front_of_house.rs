1023a1d19272677afb939fba101d7622c2938030
Jeremiah <j.munson225@gmail.com>
2020-01-10 09:18:57 -0700

    Finished chapter 7

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,3 @@
+// The front_of_house file gets the definitions from
+// the body of the front_of_house module
+pub mod hosting;
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,8 +1,7 @@
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+// Using a semicolon after 'mod front_of_house' rather than
+// using a block tells Rust to load the contents of the
+// module from another file with the same name as the module.
+mod front_of_house;
 
 // Absolute
 //use crate::front_of_house::hosting;
