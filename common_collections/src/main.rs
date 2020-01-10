56ce7dd4e187ce95141c9206c428eddf105dd25b
Jeremiah <j.munson225@gmail.com>
2020-01-10 11:11:34 -0700

    Got up to indexing into strings

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -145,7 +145,145 @@ fn main() {
         an enum plus a 'match' expression means that Rust will ensure at compile
         time that every possible case is handled.
     */
-
     // THERE ARE MORE WAYS TO USE VECTORS SO CHECK THE API DOCUMENTATION
+
+
+    /*
+        New Rustaceans commonly get stuck on strings for a combination of three
+        reasons: Rust's propensity for exposing possible errors, strings being a
+        more complicated data structure than many programmers give them credit
+        for, and UTF-8. These factors combine in a way that can seem difficult when 
+        you’re coming from other programming languages.
+
+        We discuss Strings in the context of collections because Strings are 
+        implemented as a collection of bytes, plus some methods to provide useful
+        functionality when those bytes are interpreted as text.
+
+        Rust has only one string type in the core language, which is the string slice
+        'str' that is usually seen in its borrowed form '&str'. The 'String' type, which
+        is provided by Rust's standard library rather than coded into the core 
+        language, is a growable, mutable, owned, UTF-8 encoded string type. When
+        Rustaceans refer to "strings" in Rust, they usually mean the 'String' and the
+        string slice '&str' types, not just one of those types. 
+
+        Rust's standard library also includes a number of other string types, such as
+        'OsString', 'OsStr', 'CString' and 'CStr'. See how all of those names all end
+        in 'String' or 'Str'? They refer to owned and borrowed variants, just like the
+        'String' and 'str' types seen previously. We won't discuss these other string
+        types in this chapter; see their API docs for more about them.
+
+        Many of the same operations available with 'Vec<T>' are available with
+        'String' as well, starting with the 'new' function to create a string.
+    */
+    let mut s = String::new();
+    /*
+        This line creates a new empty string called s which we can then load data
+        into. Often, we'll have some initial data that we want to start with. For
+        that, we use the 'to_string' method, which is available on any type that
+        implements the 'Display' trait, as string literals do.
+    */
+    let data = "initial contents";
+    let s = data.to_string();
+    // the mothod also works on a literal directly;
+    let s = "initial contents".to_string();
+    /*
+        This code creates a string containing 'initial contents' (without the quotes). We
+        can also use the function 'String::from' to create a String from a string
+        literal. The code below is equivalent to the code above.
+    */
+    let s = String::from("initial contents");
+    /*
+        Because strings are used for so many things, we can use many different generic 
+        APIs for strings, providing us with a lot of options. Some of them can seem 
+        redundant, but they all have their place. Here, 'String::from' and 'to_string' do
+        the same thing, so which you choose is a matter of style.
+
+        Remember that strings are UTF-8 encoded, so we can include any properly encoded
+        data in them, as shown below.
+    */
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+
+
+    /*
+        A 'String' can grow in size and its contents can change, just like the contents of
+        'Vec<T>', if you push more data into it. In addition, you can conveniently use the
+        '+' operator or the 'format!' macro to concatenate 'String' values.
+
+        We can grow a 'String' by using the 'push_str' method to append a string slice.
+    */
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s is: {}", s);
+    /*
+        The 'push_str' method takes a string slice because we don't necessarily want to take
+        ownership of the parameter. For example, the code below shows that it would be
+        unfortunate if we weren't able to use 's2' after appending its contents to 's1'
+    */
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is: {}", s2);
+    /*
+        The 'push' method takes a single character as a parameter and adds it to the
+        'String'. Remember, a character only has single quotes.
+    */
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("s is: {}", s);
+
+    /*
+        Often you'll want to combine two existing strings. One way is to use the
+        '+' operator
+    */
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    println!("s3 is: {}",s3);
+    /*
+        The reason 's1' is no longer valid after the addition and the reason we used a
+        reference to 's2' has to do with the signature of the method that gets called when
+        we use the '+' operator. The '+' operator uses the 'add' method, whose signature
+        looks like this:
+
+        fn add(self, s: &str) -> String {}
+        
+        This isn't exactly how it is implemented, but it's a good way of thinking like it now
+
+        Also note that the function wants type '&str' but '&s2' is type '&String'. The compiler
+        can coerce the '&String' argument into a '&str'. When we call the 'add' method, Rust 
+        uses a _deref coercion_, which here turns '&s2' into '&s2[..]'. This will be discussed
+        more in Chapter 15.
+
+        We can also add multiple strings at once.
+    */
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s is: {}", s);
+    /*
+        We can also use the 'format!' macro which easier to read and doesn't take ownership of
+        any of its parameters.
+    */
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    /*
+        
+    */
+
 }
 
