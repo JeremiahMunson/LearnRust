a59df23419867e5d2ca6b4fbf08a5800998b1f83
Jeremiah <j.munson225@gmail.com>
2020-01-10 13:58:48 -0700

    Worked through hash maps finishing chapter 8 except the summary.

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -382,5 +382,176 @@ fn main() {
     for b in hello5.bytes() {
         println!("{}", b);
     }
-}
 
+
+
+    /*
+        Lastly, we'll cover the _hash map_. The type 'HashMap<K,V>' stores a
+        mapping of keys of type 'K' to values of type 'V'. It does this via a
+        _hashing function_, which determines how it places these keys and values
+        into memory. Many programming languages support this kind of data
+        structure, but they often use a different name: hash, map, object, hash
+        table, dictionary, or associative array to name a few.
+
+        Hash maps are useful when you want to look up data not by using an
+        index, as you can with vectors, but by using a key that can be of any
+        type. For example, in a game, you could keep track of each team's score
+        in a hash map in which each key is a team's name and the values are each
+        team's score. Given a team name, you can retrieve its score.
+
+        You can create an empty hash map with 'new' and add elements with
+        'insert'. In the code below, we're keeping track of the scores of two
+        teams whose names are Blue and Yellow. The Blue team starts with 10 
+        points, and the Yellow team starts with 50.
+    */
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    /*
+        Note we need to bring HashMap into scope with 'use'. Of the three
+        collections covered here, HashMap is the least often used and have less
+        support from the standard library; there's no built-in macro to construct
+        them, for example.
+
+        This 'HashMap' has keys of type 'String' and values of type 'i32'. Like
+        vectors, hash maps are homogeneous: all of the keys must have the same
+        type, and all of the values must have the same type.
+
+        Another way of constructing a hash map is by using the 'collect' method on
+        a vector of tuples, where eac tuple consists of a key and its value. The
+        'collect' method gathers data into a number of collection types, including
+        'HashMap'. For example, if we had the team names and initial scores in two
+        separate vectors, we could use the zip method to create a vector of
+        tuples where "Blue" is paired with 10, and so forth. Then we could use the
+        'collect' method to turn that vector of tuples into a hash map, as shown
+        below.
+    */
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    /*
+        The type annotation 'HashMap<_, _>' is needed here because it's possible
+        to 'collect' into many different data structures and Rust doesn't know
+        which you want unless you specify. For the parameters for the key and
+        value types, however, we used underscores, and Rust can infer the 
+        types that the hash map contains based on the types of the data in the
+        vectors.
+
+        For types that implement the 'Copy' trait, like 'i32', the values are
+        copied into the hash map. For owned values like 'String', the values will
+        be moved and the hash map will be the owner of those values, as
+        demonstrated below.
+    */
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point
+    /*
+        If we insert references to values into the hash map, the values won’t be 
+        moved into the hash map. The values that the references point to must be 
+        valid for at least as long as the hash map is valid. We’ll talk more about
+        these issues in Chapter 10.
+
+        We can get a value out of the hash map by providing its key to the
+        'get' method, as shown below.
+    */
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    match score {
+        Some(i) => println!("score is: {}", i),
+        None => println!("No team with that name"),
+    };
+    /*
+        Here, 'score' will have the value that's associated with the Blue team, and
+        the result will be 'Some(&10)'. The result is wrapped in 'Some' because
+        'get' returns an 'Option<&V>'; if there's no value for that key in the hash
+        map, get will return 'None'.
+
+        We can iterate over each key/value pair in a hash map in a similar manner as
+        we do with vectors, using a 'for' loop:
+    */
+    for (key,value) in &scores {
+        println!("{}: {}", key, value);
+    }
+    /*
+        Although the number of keys and values is growable, each key can
+        only have one value associated with it at a time. When you want
+        to change the data in a hash map, you have to decide how to
+        handle the case when a key already has a value assigned. You
+        could replace the old value with the new value, completely 
+        disregarding the old value. You could keep the old value and
+        ignore the new value, only adding the new value if the key
+        _doesn't_ already have a value. Or you could combine the old
+        value and the new value. Let's look at how to do each of these.
+
+        If we insert a key and a value into a hash map and then insert
+        that same key with a different value, the value associated with
+        that key will be replaced. Even though the code below calls
+        'insert' twice, the hash map will only contain one key/value pair
+        because we're inserting the value for the Blue team's key both
+        times.
+    */
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+    /*
+        It's common to check whether a particular key has a value and, if
+        it doesn't, insert a value for it. Hash maps have a special API
+        for this called 'entry' that takes the key you want to check as
+        a parameter. The return value of the 'entry' method is an
+        enum called 'Entry' that represents a value that might or might 
+        not exist. Let's say we want to check whether the key for the
+        Yellow team has a value associated with it. If it doesn't, we
+        want to insert the value 50, and the same for the Blue 
+        team. Using the 'entry' API, the code looks like...
+    */
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+    /*
+        The 'or_insert' method on 'Entry' is defined to return a mutable
+        reference to the value for the corresponding 'Entry' key if that
+        key exists, and if not, inserts the parameter as the new
+        value for this key and returns a mutable reference to the new
+        value. This technique is much cleaner than writing the logic
+        ourselves and, in addition, plays more nicely with the borrow
+        checker.
+
+        Another common use case for hash maps is to look up a key's 
+        value and then update it based on the old value. For 
+        instance, the code below counts how many times each word
+        appears in some text. We use a hash map with the words as keys and
+        increment the value to keep track of how many times we've seen
+        that word. If it's the first time we've seen a word, we'll first
+        insert the value 0.
+    */
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+    /*
+        The 'or_insert' method actually returns a mutable reference 
+        ('&mut V') to the value for this key. Here we store that mutable
+        reference in the count variable, so in order to assign to that
+        value, we must first dereference 'count' using the asterisk.
+
+        By default, 'HashMap' uses a "cryptographically strong" hashing
+        function that can provide resistance to Denial of Service (DOS)
+        attacks. This is not the fastest hashing algorithm avaiable, but
+        the trade-off for better security that comes with the drop in
+        performance is worth it.
+    */
+}
